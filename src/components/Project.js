337690d8be13f2af39be928ee8204e31f9efe9ed
Aidan Loughney <loughneyaidan@gmail.com>
2023-04-25 19:44:59 -0400

    project section more fleshed out, need to make it responsive for large screens

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function Project(props) {
   return (
-    <div>{props.info.name}</div>
+    <div className="flex flex-col justify-between flex-grow px-8 py-8 space-y-8 shadow-2xl rounded-3xl bg-primary">
+    <div className="flex flex-row space-x-8 ">
+    <div className="text-2xl font-bold text-white self-left md:text-4xl">
+        {props.info.name}
+    </div>
+    <div className="self-center">
+    {props.info.src_link.length > 1 ? 
+          <a  href={props.info.src_link}>
+          <svg className=" fill-white hover:fill-hover" xmlns="http://www.w3.org/2000/svg" width="48" height="48" 
+          viewBox="0 0 24 24"></svg>
+          </a>
+          :
+          <a href="mailto:loughneyaidan@gmail.com?subject=Requesting {props.info.name} source code">
+          <svg className="fill-white hover:fill-hover" xmlns="http://www.w3.org/2000/svg" width="48" height="48"  viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+          </svg>
+          </a>
+        }
+          </div>
+    </div>
+    <div className="flex-grow text-xl font-bold text-white md:text-2xl">
+      {props.info.summary}
+          </div>
+    <div className="flex self-center my-auto space-x-8 text-4xl text-white h-1/4">
+          <a className="self-center text-sm border rounded-lg bg-secondary" href="#" >
+          See More
+          </a>
+        
+    </div>
+</div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,28 +4,60 @@ import Project from './Project'
 export default function Projects() {
   const projects = [
     {
+      "id" : 1,
       "name" : "Todo",
       "date" : "dd/mm/yy",
       "summary" : "Laboris veniam dolore cupidatat et nostrud amet ad lorem aliqua ipsum officia dolore velit ad pariatur. Qui nostrud adipiscing deserunt esse ex laboris commodo irure dolor sint duis occaecat",
       "skills" : ['HTML', 'CSS', 'JavaScript'],
-      "header-img" : "#",
-      "demo-link" : "#",
-      "src-link" : "#",
+      "header_img" : "#",
+      "demo_link" : "https://todo-dashboard.netlify.app/",
+      "src_link" : "https://github.com/ArLough/todo",
     },
     {
+      "id" : 2,
       "name" : "TuneBot",
       "date" : "dd/mm/yy",
       "summary" : "Laboris veniam dolore cupidatat et nostrud amet ad lorem aliqua ipsum officia dolore velit ad pariatur. Qui nostrud adipiscing deserunt esse ex laboris commodo irure dolor sint duis occaecat",
       "skills" : ['HTML', 'CSS', 'JavaScript'],
-      "header-img" : "#",
-      "demo-link" : "#",
-      "src-link" : "#",
+      "header_img" : "#",
+      "demo_link" : "#",
+      "src_link" : "https://github.com/mumichians/TuneBot-WebApp",
+    },
+    {
+      "id" : 3,
+      "name" : "Insta485",
+      "date" : "/mm/yy",
+      "summary" : "Built a social networking web application similar to Instagram with client-side and server-side dynamic pages as part of EECS 485: Web Systems at the University of Michigan",
+      "skills" : ['Restful APIs', 'React.js', 'Python', 'Flask', 'SQLite'],
+      "header_img" : "#",
+      "demo_link" : "#",
+      "src_link" : "",
+    },
+    {
+      "id" : 4,
+      "name" : "Portfolio Website V2",
+      "date" : "dd/mm/yy",
+      "summary" : "Laboris veniam dolore cupidatat et nostrud amet ad lorem aliqua ipsum officia dolore velit ad pariatur. Qui nostrud adipiscing deserunt esse ex laboris commodo irure dolor sint duis occaecat",
+      "skills" : ['HTML', 'CSS', 'JavaScript'],
+      "header_img" : "#",
+      "demo_link" : "#",
+      "src_link" : "https://github.com/ArLough/Personal-Website-V2",
+    },
+    {
+      "id" : 5,
+      "name" : "Dummy",
+      "date" : "dd/mm/yy",
+      "summary" : "Laboris veniam dolore cupidatat et nostrud amet ad lorem aliqua ipsum officia dolore velit ad pariatur. Qui nostrud adipiscing deserunt esse ex laboris commodo irure dolor sint duis occaecat",
+      "skills" : ['HTML', 'CSS', 'JavaScript'],
+      "header_img" : "#",
+      "demo_link" : "#",
+      "src_link" : "#",
     }
   ];
   return (
     <div>
       <div>Projects</div>
-      <div className='flex space-x-6'>
+      <div className='flex flex-col h-auto mt-20 ml-20 mr-20 space-x-12 space-y-14 lg:grid lg:grid-cols-2 2xl:grid-cols-3'>
       {projects.map(project =>(
         <Project info={project}> </Project>
       ))}
