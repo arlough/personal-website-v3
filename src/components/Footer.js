29c9797deabda918e464fa7fd4d910fd5bca86d8
Aidan Loughney <loughneyaidan@gmail.com>
2023-05-05 20:52:34 -0400

    style hover improvements, and added meta tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
    <footer>
-    <div className='w-full py-10 mt-20 text-center text-white rounded-l bg-background'>
+    <div className='w-full py-10 mt-20 text-lg text-center text-white rounded-l bg-background'>
         © 2023 Aidan Loughney
     </div>
    </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,19 +9,22 @@ export default function Navbar(){
                     <div className='flex px-5 py-2 space-x-5'>
                     {/* right nav */}
                     <div className='flex items-center space-x-5 '>
-                        <Link href="/" className='px-3 py-2 hover:text-hover'>
+                        <Link href="/" className='px-3 py-2 transition duration-300 group hover:text-hover'>
                             Home
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-hover"></span>
                         </Link>
-                        <Link href="/projects" className='px-3 py-2 hover:text-hover'>
+                        <Link href="/projects" className='px-3 py-2 transition duration-300 group hover:text-hover'>
                             Projects
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-hover"></span>
                         </Link>
                     </div>
                 </div>
                 
                 {/* left nav */}
                 <div className='flex items-center'>
-                    <Link href="/contact" className='px-5 py-2 hover:text-hover'>
-                        Contact
+                    <Link href="/contact" className='px-5 py-2 transition duration-300 group hover:text-hover'>
+                            Contact
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-hover"></span>
                     </Link>
                 </div>
                 </div>
