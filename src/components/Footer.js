c9ca06c631829e7bd953a7b89b941e846f4e44f6
Aidan Loughney <loughneyaidan@gmail.com>
2024-05-19 14:43:08 -0600

    tweak footer year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
    <footer className='w-full py-5 mt-auto text-sm text-center '>
-        © 2023 Aidan Loughney · <a className="underline hover:text-hover" href="colophon">colophon</a>
+        © 2024 Aidan Loughney · <a className="underline hover:text-hover" href="colophon">colophon</a>
    </footer>
   )
 }
