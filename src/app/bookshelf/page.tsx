0b5ce2dee5a9930aa60fee0d3c8b5354d6dcf3d5
Aidan Loughney <loughneyaidan@gmail.com>
2024-05-19 15:20:24 -0600

    minor fixes & groundwork for bookshelf page

--- src/app/bookshelf/page.tsx
+++ src/app/bookshelf/page.tsx
@@ -1,18 +1,33 @@
 import { Metadata } from "next";
 import { Section } from "../../components/Section";
 import { StandardLayout } from "../../components/layouts/StandardLayout";
+import { useEffect, useState } from "react";
 export const metadata: Metadata = {
   title: "Bookshelf - Aidan Loughney",
 };
 export default function Bookshelf() {
+    // const [currentlyReading, setCurrentlyReading] = useState([]);
+    // const [recentlyRead, setRecentlyRead] = useState([]);
+    // const [favorites, setFavorites] = useState([]);
+    // const [readingList, setReadingList] = useState([]);
+
+    // useEffect(() => {
+    //   async function fetchData() {
+    //     const response = await fetch('/api/data');
+    //     const result = await response.json();
+    //   }   
+    //   fetchData();
+    // }, []);
+
   return (
     <StandardLayout title={"Bookshelf"}>
       <Section>
         <p>
-            My digital book shelf :)
+            My digital book shelf 📚 :)
         </p>
       </Section>
-      <Section title="Read this year">under construction</Section>
+      <Section title="Currently reading">under construction</Section>
+      <Section title="Recently read">under construction</Section>
       <Section title="Favorites">under construction</Section>
       <Section title="Reading list">under construction</Section>
     </StandardLayout>
