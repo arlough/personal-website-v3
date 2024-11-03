d1f2740efe6f5b65f29884f9a4850df638133821
Aidan Loughney <loughneyaidan@gmail.com>
2024-11-02 22:47:46 -0600

    link icon size

--- src/components/atoms/ExtLink.tsx
+++ src/components/atoms/ExtLink.tsx
@@ -16,7 +16,7 @@ export default function ExtLink({ href, children, hideIcon }: ExtLinkProps) {
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						strokeWidth={2.0}
+						strokeWidth={2.5}
 						stroke="currentColor"
 						className="size-2"
 					>
