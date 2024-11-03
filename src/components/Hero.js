c9ff6239f803482bf0706e725d7fadacbf5e6764
Aidan Loughney <loughneyaidan@gmail.com>
2024-11-02 22:50:54 -0600

    remove framer on hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,60 +1,37 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 export default function Hero() {
-	return (
-		<section className="flex flex-col justify-center ml-auto mr-auto sm:flex-row">
-			<motion.div
-				className="sm:px-8 "
-				initial={{ scale: 0.75 }}
-				animate={{ scale: 1 }}
-				transition={{ ease: "easeOut", duration: 0.5 }}
-			>
-				<Image
-					priority={true}
-					width={200}
-					height={250}
-					placeholder="blur"
-					className=" rounded-3xl"
-					src="/profile_pic.jpg"
-					alt="profile picture"
-				/>
-			</motion.div>
+  return (
+    <section className="flex flex-col justify-center ml-auto mr-auto sm:flex-row">
+      <div>
+        <Image
+          priority={true}
+          width={200}
+          height={250}
+          placeholder="blur"
+          className=" rounded-3xl"
+          src="/profile_pic.jpg"
+          alt="profile picture"
+        />
+      </div>
 
-			<div
-				id="hero"
-				className="flex flex-col justify-center py-8 space-y-3 sm:px-8 max-w-fit "
-			>
-				<motion.h1
-					initial={{ x: 100 }}
-					animate={{ x: 0 }}
-					transition={{ type: "spring" }}
-					className="text-4xl font-black text-left"
-				>
-					Aidan Loughney
-				</motion.h1>
-				<motion.p
-					initial={{ x: 100 }}
-					animate={{ x: 0 }}
-					transition={{ type: "spring" }}
-					className="text-xl font-bold text-left"
-				>
-					Hey there!{" "}
-					<span className="inline-block animation-waving-hand">👋🏻</span>
-				</motion.p>
+      <div
+        id="hero"
+        className="flex flex-col justify-center py-8 space-y-3 sm:px-8 max-w-fit "
+      >
+        <h1 className="text-4xl font-black text-left">Aidan Loughney</h1>
+        <h2 className="text-xl font-bold text-left">
+          Hey there!{" "}
+          <span className="inline-block animation-waving-hand">👋🏻</span>
+        </h2>
 
-				<motion.p
-					initial={{ x: 100 }}
-					animate={{ x: 0 }}
-					transition={{ type: "spring" }}
-					className="text-xl font-bold text-left text-balance "
-				>
-					I&apos;m Aidan, a Software Engineer <br></br> based out of Salt Lake
-					City <span className="inline-block hover:animate-jump">⛰️</span>
-				</motion.p>
-			</div>
-		</section>
-	);
+        <h2 className="text-xl font-bold text-left text-balance ">
+          I&apos;m Aidan, a Software Engineer <br></br> based out of Salt Lake
+          City <span className="inline-block hover:animate-jump">⛰️</span>
+        </h2>
+      </div>
+    </section>
+  );
 }
