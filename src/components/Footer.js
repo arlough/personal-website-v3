f4c4fbc77f9027d4d0d0e3a12af591849d074907
Aidan Loughney <loughneyaidan@gmail.com>
2025-01-01 19:15:13 -0500

    2025

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "next-view-transitions";
 
 export default function Footer() {
-	return (
-		<footer className="w-full py-5 mt-auto text-sm text-center ">
-			© 2024 Aidan Loughney ·{" "}
-			<Link className="underline hover:text-hover" href="colophon">
-				colophon
-			</Link>
-		</footer>
-	);
+  return (
+    <footer className="w-full py-5 mt-auto text-sm text-center ">
+      © 2025 Aidan Loughney ·{" "}
+      <Link className="underline hover:text-hover" href="colophon">
+        colophon
+      </Link>
+    </footer>
+  );
 }
