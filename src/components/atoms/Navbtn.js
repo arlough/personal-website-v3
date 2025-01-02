bfe9801a2318c4ed0c167f989f1ffb36ddc41e47
Aidan Loughney <loughneyaidan@gmail.com>
2025-01-01 19:31:27 -0500

    fix tab highlight

--- src/components/atoms/Navbtn.js
+++ src/components/atoms/Navbtn.js
@@ -4,24 +4,24 @@ import { Link } from "next-view-transitions";
 import { usePathname } from "next/navigation";
 
 export default function Navbtn({ link, label, onClick }) {
-	const pathname = usePathname();
-	return (
-		<Link
-			onClick={() => {
-				onClick && onClick();
-			}}
-			href={link}
-			className="px-3 py-2 group hover:text-hover"
-		>
-			{label}
-			<span
-				className={
-					pathname.replace("/", "") === label.toLowerCase() ||
-					(label === "Home" && pathname.replace("/", "") === "")
-						? "block max-w-full h-0.5 bg-white group-hover:bg-hover"
-						: "block max-w-0 group-hover:max-w-full  h-0.5 bg-hover"
-				}
-			></span>
-		</Link>
-	);
+  const pathname = usePathname();
+  return (
+    <Link
+      onClick={() => {
+        onClick && onClick();
+      }}
+      href={link}
+      className="px-3 py-2 group hover:text-hover"
+    >
+      {label}
+      <span
+        className={
+          pathname.replace("/", "") === label.toLowerCase() ||
+          (label === "Home" && pathname.replace("/", "") === "")
+            ? "block max-w-full h-0.5 bg-hover group-hover:bg-hover"
+            : "block max-w-0 group-hover:max-w-full  h-0.5 bg-hover"
+        }
+      ></span>
+    </Link>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      fontFamily: {'sans':['-apple-system-ui-serif', "ui-serif", 'Georgia', "serif"]},
+      fontFamily: {
+        sans: ["-apple-system-ui-serif", "ui-serif", "Georgia", "serif"],
+      },
       colors: {
-        'primary': 'var(--background-start-rgb)',
-        'primary': 'var(--primary)',
-        'secondary': 'var(--secondary)',
-        'hover': 'var(--hover)',
-        'button': 'var(--button)',
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        hover: "var(--hover)",
+        button: "var(--button)",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
         wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
         },
         jump: {
-          '0%': { transform: 'translateY(0%)' },
-          '25%': { transform: 'translateY(-10%)' },
-          '50%': { transform: 'translateY(-20%)' },
-          '60%': { transform: 'translateY(-10%)' },
-          '70%': { transform: 'translateY(-5%)' },
-          '80%': { transform: 'translateY(0)' },
+          "0%": { transform: "translateY(0%)" },
+          "25%": { transform: "translateY(-10%)" },
+          "50%": { transform: "translateY(-20%)" },
+          "60%": { transform: "translateY(-10%)" },
+          "70%": { transform: "translateY(-5%)" },
+          "80%": { transform: "translateY(0)" },
         },
       },
       animation: {
-        'waving-hand': 'wave 2s linear 2',
-        'jump': 'jump 0.5s ease-in 2',
+        "waving-hand": "wave 2s linear 2",
+        jump: "jump 0.5s ease-in 2",
       },
     },
   },
   plugins: [],
-}
+};
