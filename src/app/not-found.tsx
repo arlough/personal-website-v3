65227cbd8d8aa83a7db6d2c2ee47c62302c554cd
Aidan Loughney <loughneyaidan@gmail.com>
2025-04-04 17:57:36 -0600

    add custom not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+    <div className="flex justify-center font-bold">
+      <div className=" flex flex-col items-center font-bold space-y-4">
+      <h1 className="text-3xl">404 - Page Not Found</h1>
+    <p className="text-md">Where were you trying to go 🤨</p>
+    </div>
+    </div>)
+  }
