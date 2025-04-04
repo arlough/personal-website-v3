12c6647dba18a22923885ed24aee106213d4974d
Aidan Loughney <loughneyaidan@gmail.com>
2025-04-04 17:58:17 -0600

    don't need onClick on navbtn

--- src/components/atoms/Navbtn.js
+++ src/components/atoms/Navbtn.js
@@ -3,13 +3,10 @@ import React from "react";
 import { Link } from "next-view-transitions";
 import { usePathname } from "next/navigation";
 
-export default function Navbtn({ link, label, onClick }) {
+export default function Navbtn({ link, label }) {
   const pathname = usePathname();
   return (
     <Link
-      onClick={() => {
-        onClick && onClick();
-      }}
       href={link}
       className="px-3 py-2 group hover:text-hover"
     >
