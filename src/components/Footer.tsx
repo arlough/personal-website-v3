1caa0e632a1d3e2de68f0b087fd27bb78a859df3
Aidan Loughney <loughneyaidan@gmail.com>
2025-04-06 10:59:46 -0600

    prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,7 @@ import InlineLink from "./atoms/InlineLink";
 export default function Footer() {
   return (
     <footer className="w-full py-5 mt-auto text-sm text-center">
-      © 2025 Aidan Loughney ·{" "}
-      <InlineLink href="colophon">
-        colophon
-      </InlineLink>
+      © 2025 Aidan Loughney · <InlineLink href="colophon">colophon</InlineLink>
     </footer>
   );
 }
