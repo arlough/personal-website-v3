3711a4b89f9bb42654203d4e0f3ced96d7c75335
Aidan Loughney <loughneyaidan@gmail.com>
2025-04-05 18:54:07 -0600

    prettier

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,16 +1,25 @@
-
 import fs from "node:fs";
 import getUrl from "../../lib/getUrl";
 import InlineLink from "../../components/atoms/InlineLink";
 
 export default function Directory() {
   const pages = fs
-  .readdirSync("src/app", { withFileTypes: true })
-  .filter((file) => file.isDirectory())
-  .map((folder) => folder.name)
-  .filter(
-    (folder) =>
-      !folder.startsWith("(") && !folder.startsWith("_") && folder !== "og",
-  ); 
-  return <div className="flex justify-center "><div className="flex justify-center space-x-6 ">{...pages.map((page) => (<InlineLink key="page" href={getUrl(page)}>{page}</InlineLink>))}</div></div>;
+    .readdirSync("src/app", { withFileTypes: true })
+    .filter((file) => file.isDirectory())
+    .map((folder) => folder.name)
+    .filter(
+      (folder) =>
+        !folder.startsWith("(") && !folder.startsWith("_") && folder !== "og",
+    );
+  return (
+    <div className="flex justify-center ">
+      <div className="flex justify-center space-x-6 ">
+        {...pages.map((page) => (
+          <InlineLink key="page" href={getUrl(page)}>
+            {page}
+          </InlineLink>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,10 @@ export default function NotFound() {
       <div className="flex flex-col items-center font-bold space-y-4">
         <h1 className="text-3xl">404 - Page Not Found</h1>
         <p className="text-lg">Where were you trying to go 🤨</p>
-        <div><InlineLink href={""}>I&aposm feeling lucky</InlineLink> · <InlineLink href={"directory"}>Directory</InlineLink></div>
+        <div>
+          <InlineLink href={""}>I&aposm feeling lucky</InlineLink> ·{" "}
+          <InlineLink href={"directory"}>Directory</InlineLink>
+        </div>
       </div>
     </div>
   );
