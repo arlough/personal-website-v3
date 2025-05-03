47dbb232b5ba784c5fee7c91f07aaf734a98a3b0
Aidan Loughney <loughneyaidan@gmail.com>
2025-05-03 15:44:08 -0600

    hide footer on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import InlineLink from "./atoms/InlineLink";
 
 export default function Footer() {
   return (
-    <footer className="w-full py-5 mt-auto text-sm text-center">
+    <footer className="hidden md:inline w-full py-5 mt-auto text-sm text-center">
       © 2025 Aidan Loughney · <InlineLink href="colophon">colophon</InlineLink>
     </footer>
   );
