48e854c393fade2e8ced5860cf89e79283c6470c
Aidan Loughney <loughneyaidan@gmail.com>
2025-07-19 14:55:25 -0600

    small style fixes

--- src/components/atoms/BlockQuote.js
+++ src/components/atoms/BlockQuote.js
@@ -1,6 +1,6 @@
 export function BlockQuote({ citation, children }) {
 	return (
-		<blockquote className="p-2 my-2 border-l-2" cite={citation}>
+		<blockquote className="p-2 my-2 border-l-2 border-primary has-hover:border-secondary" cite={citation}>
 			<div className="ml-5 italic ">{children}</div>
 		</blockquote>
 	);
